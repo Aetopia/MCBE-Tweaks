818bd75c7716a300a8cb2094871c00dca427ae1a
Shanki <81349208+aShanki@users.noreply.github.com>
2021-11-04 15:43:47 +0800

    Update optionspatcher.bat

--- scripts (remove or move to another repo later)/optionspatcher.bat
+++ scripts (remove or move to another repo later)/optionspatcher.bat
@@ -1,8 +1,7 @@
 @echo off
-echo Closing Minecraft...
-
-taskkil /IM Minecraft.Windows.exe
-
-
-
+echo This script is going to kill minecraft, apply optimised settings and sign you out of xbox press enter to proceed
 pause
+echo Proceeding
+tasklist | find /i "Minecraft.Windows.exe" && taskkill /im Minecraft.Windows.exe /F || echo process "Minecraft.Windows.exe" not running.
+pause
+
