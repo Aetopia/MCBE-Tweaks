e14c71d3457d1082924d3dd1e8b9bf52a66147bb
Shanki <81349208+aShanki@users.noreply.github.com>
2021-11-04 16:11:14 +0800

    Delete optionspatcher.bat

--- scripts (remove or move to another repo later)/optionspatcher.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo This script is going to kill minecraft, apply optimised settings and sign you out of xbox press enter to proceed
-pause
-echo Proceeding
-tasklist | find /i "Minecraft.Windows.exe" && taskkill /im Minecraft.Windows.exe /F || echo process "Minecraft.Windows.exe" not running.
-pause
-
